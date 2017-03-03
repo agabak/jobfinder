dae9da02a72a2a11a2229e633ff3ef16a6d28f58
Agaba Kabunga <Agaba Kabunga>
2017-03-02 22:10:14 -0600

    use service

--- app/main/mainController.js
+++ app/main/mainController.js
@@ -1,16 +1,18 @@
-(function(){
-   'use strict';
-    
+(function () {
+    'use strict';
+
     angular.module('jobfinder')
-           .controller('mainController',mainController);
-    
-    mainController.$inject = ['$resource'];
-    
-    function mainController($resource){
+        .controller('mainController', mainController);
+
+    mainController.$inject = ['$resource','jobService'];
+
+    function mainController($resource, jobService) {
         var vm = this;
-        
-        vm.title =  "Find a Job today !"
-        vm.jobs = $resource('/api/jobs').query();
+
+        vm.title = "Find a Job today !"
+         jobService.getJobs().then(function(response){
+              vm.jobs =   response.data
+        })
     }
-    
+
 }());
---
+++ app/service/jobService.js
@@ -0,0 +1,19 @@
+(function () {
+    'use strict';
+
+    angular.module('jobfinder')
+        .factory('jobService', jobService);
+
+    jobService.$inject = ['$http'];
+
+    function jobService($http) {
+        return {
+            getJobs: getJobs
+        };
+        
+        function getJobs(){         
+            return $http.get('/api/jobs');
+        }
+    };
+    
+}());
