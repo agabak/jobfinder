41506083acc209dc49c69fcd8c49f40a3b83b15d
Kabunga <Agaba.Kabunga@nov.com>
2017-03-03 14:44:41 -0600

    add test

--- test/jod-data-spec.js
+++ test/jod-data-spec.js
@@ -1,8 +1,8 @@
-var assert = require('assert')
-var expect = require('chai').expect;
-var mongoose = require('mongoose');
-var jobModel = require('../models/job');
-var  mongooseLab = 'mongodb://agaba:gabby123@ds133328.mlab.com:33328/agabajob';
+var assert = require('assert'),
+    expect = require('chai').expect,
+    mongoose = require('mongoose'),
+    jobModel = require('../models/job'),
+    mongooseLab = 'mongodb://agaba:gabby123@ds133328.mlab.com:33328/agabajob';
 
 describe("get jobs",function(){
 it("should never be  empty since are seeded",function(done){	
