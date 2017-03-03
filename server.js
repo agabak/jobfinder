0fac6dff64496f41080e88e460740d148a6a825f
Agaba Kabunga <Agaba Kabunga>
2017-03-02 21:33:36 -0600

    add the database

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 (function () {
     'use strict';
-    var express = require('express')
-        , path = require('path')
-        , port = process.env.PORT || 8000
-        , app = express();
+    var express = require('express'),
+        path = require('path'),
+        port = process.env.PORT || 8000,
+        mongoose = require('mongoose'),
+        jobModel =  require('./models/job'),
+        app = express();
     app.use(express.static(path.join(__dirname)));
+    
+    app.get('/api/jobs',function(req,res){
+        mongoose.model('Job').find({}).exec(function(error,collection){
+            if(error){
+                console.log(error);
+            }else{
+            res.send(collection);
+            }
+        });
+    });
+    
     app.get('/', function (req, res) {
         res.sendStatus(200);
     });
+    
+    var localMongoose = 'mongodb://localhost:27017/jobfinder';
+    var mongooseLab = 'mongodb://agaba:gabby123@ds133328.mlab.com:33328/agabajob';
+
+    mongoose.connect(mongooseLab, function(){
+        jobModel.seedJobs();
+        console.log('connect  to mongoose db succeful !');
+    });
+   
     app.listen(port, function (err) {
         console.log('Running at port:' + port);
     });
